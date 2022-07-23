ff3c03e060ed2485e9f481cc52c6fd64761e6e0c
1shaker <salmazini@students.iugaza.edu.ps>
2022-07-23 23:06:56 +0300

    Shaker

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('test_test', function (){
+    dd('yes');
+});
+
+Route::get('tojar',[studendController::class,'index']);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Tojars\TojarController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::get('/', static function (){
+    return view('tojars.index');
 });
+
+Route::get('add', static function (){
+    return view('tojars.add');
+});
+
+
+Route::get('tojar/create',[TojarController::class,'create'])->name('tojar/create');
+Route::post('tojar_store',[TojarController::class,'store'])->name('tojar_store');
+Route::get('tojar',[TojarController::class,'index'])->name('tojar');
+Route::get('tojar_edit::46779::5{id}18::6798',[TojarController::class,'edit'])->name('tojar_edit::46779::5{id}18::');
+Route::put('tojar_upDate{id}',[TojarController::class,'update'])->name('tojar_upDate{id}');
+Route::post('tojar_delete{id}',[TojarController::class,'destroy'])->name('tojar_delete{id}');
+
+
